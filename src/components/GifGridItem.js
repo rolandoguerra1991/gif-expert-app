02579f216bca3a0a72126e0fcbd4d547c599ffee
Rolando Guerra <rolandoguerra1991@gmail.com>
2022-05-26 15:23:39 -0400

    animated css

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function GifGridItem({ id, title, url }) {
   return (
-    <div className='card'>
+    <div className='card animate__animated animate__headShake'>
         <img src={url} alt={title} />
         <p>{title}</p>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import GifExpertApp from './GifExpertApp';
 import './index.css';
+import 'animate.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<GifExpertApp />);
