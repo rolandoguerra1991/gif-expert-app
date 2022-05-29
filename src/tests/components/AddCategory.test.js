4e34bfd3c00a7db104813307b8ad16bf91c49da5
Rolando Guerra <rolandoguerra1991@gmail.com>
2022-05-29 18:05:27 -0400

    debe de llamar setCategories y limpiar la caja de texto

--- src/tests/components/AddCategory.test.js
+++ src/tests/components/AddCategory.test.js
@@ -28,4 +28,17 @@ describe('Pruebas en <AddCateogry />', () => {
     wrapper.find('form').simulate('submit', { preventDefault() {} });
     expect(setCategories).not.toHaveBeenCalled();
   });
+
+  test('debe de llamar setCategories y limpiar la caja de texto', () => { 
+    // 1. simular el inputValue
+    wrapper.find('input').simulate('change', {target : { value: "Hola mundo" } });
+    // 2. simular el submit
+    wrapper.find('form').simulate('submit', {preventDefault(){}});
+    // 3. setCategories se debe de haber llamado
+    expect(setCategories).toHaveBeenCalled();
+    expect(setCategories).toHaveBeenCalledTimes(1);
+    expect(setCategories).toHaveBeenCalledWith(expect.any(Function));
+    // 4. el valor del input debe de estar ''
+    expect(wrapper.find('input').prop('value')).toBe('');
+  });
 })
